600e91834e3fddbd1ca5042e9f3cc53ea64a88cb
Mahendra AR <mahendra0859@gmail.com>
2020-11-04 17:07:13 +0530

    Customization of MUI Components and Theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import {
+  createMuiTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from '@material-ui/core';
 import './App.css';
-import { SideMenu, Header } from './components';
+import { SideMenu, Header, PageHeader } from './components';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126',
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526',
+    },
+    background: {
+      default: '#f4f5fd',
+    },
+  },
+  shape: {
+    borderRadius: '12px',
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: 'translateZ(0)',
+      },
+    },
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -12,13 +49,18 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+          title='Page Header'
+          subTitle='Page Description'
+          icon={<PeopleOutlineIcon fontSize='large' />}
+        />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SideMenu } from './SideMenu/index';
 export { default as Header } from './Header/index';
+export { default as PageHeader } from './PageHeader/index';
